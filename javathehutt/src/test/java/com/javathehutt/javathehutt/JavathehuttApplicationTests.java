6b95ad4200165387f50f6479c49dfb5209e351af
= <sbouzoubaa@uoc.edu>
2021-11-05 18:48:12 +0100

    spring

---
+++ javathehutt/src/test/java/com/javathehutt/javathehutt/JavathehuttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javathehutt.javathehutt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavathehuttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
