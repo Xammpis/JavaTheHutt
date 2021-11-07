fb82bf3df9223fd7a8f95b4cbe72698c3de1c24e
= <sbouzoubaa@uoc.edu>
2021-11-07 13:55:32 +0100

    Creación primera clase "ONG"

---
+++ javathehutt/src/main/java/com/javathehutt/ONG/ONG.java
@@ -0,0 +1,13 @@
+package com.javathehutt.ONG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ONG {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ONG.class, args);
+	}
+
+}
